a6387badf4495838ac86f1ec39a288f5d69db500
Gustavo <duartegusfalcao@gmail.com>
2025-05-22 09:22:13 -0300

    Adicionado dependencia para trabalhar com arquivo Json.

--- src/main/java/assessoria/Main.java
+++ src/main/java/assessoria/Main.java
@@ -1,7 +1,13 @@
 package assessoria;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.LinkedHashMap;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Assessoria-Corrida");
+
     }
 }
