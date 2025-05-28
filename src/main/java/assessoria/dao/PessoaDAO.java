29e1196980a5904a69edc7703e187ccbf90002c1
Gabriel Felipe <gabri26.cost@gmail.com>
2025-05-28 09:48:16 -0300

    Pessoa(Controller, DAO). Aluno(Controller, DAO). inserir em Pessoas formato padrão.

--- src/main/java/assessoria/dao/PessoaDAO.java
+++ src/main/java/assessoria/dao/PessoaDAO.java
@@ -1,6 +1,5 @@
 package assessoria.dao;
 
-import assessoria.model.Aluno;
 import assessoria.model.Pessoa;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -9,16 +8,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
-public class AlunoDAO extends PessoaDAO {
-
-    String nome = "aluno";
+public class PessoaDAO {
+    String nome = "pessoas";
     private final String arquivoUsuarios = "src/main/java/assessoria/util/users/" + nome + ".json";
 
-    public void inserirAlunoNoArquivo(Map<String, Aluno> alunoMap) {
+    public void inserirPessoaNoArquivo(Map<String, Pessoa> PessoaMap) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         try{
-            objectMapper.writeValue(new File(arquivoUsuarios), alunoMap);
+            objectMapper.writeValue(new File(arquivoUsuarios), PessoaMap);
             System.out.println("Arquivo escrito com sucesso");
         } catch (IOException e) {
             System.out.println("Erro ao escrever no arquivo");
